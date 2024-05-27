97e520a9c9d8fed711ea7c0f25d8d9a251e65e10
3snanugraha <trisnanugraha87@gmail.com>
2024-05-27 13:21:53 +0700

    tambah pelanggan

--- App/view/modals.php
+++ App/view/modals.php
@@ -59,6 +59,59 @@
 </div>
 <!-- End modal -->
 
+<!-- Modal Tambah Pelanggan -->
+<div class="modal fade" id="tambah-data-pelanggan" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Tambah Pelanggan</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="Controller.php" method="post">
+          <div class="row mt-2">
+            <div class="col-3">
+              <label for="nama_pelanggan"> Nama Pelanggan </label>
+            </div>
+            <div class="col-9">
+              <input type="text" class="form-control rounded-pill" name="nama_pelanggan" required>
+            </div>
+          </div>
+          <div class="row mt-2">
+            <div class="col-3">
+              <label for="no_hp">No. HP</label>
+            </div>
+            <div class="col-9">
+              <input type="text" class="form-control rounded-pill" name="no_hp" required>
+            </div>
+          </div>
+          <div class="row mt-2">
+            <div class="col-3">
+              <label for="alamat">Alamat</label>
+            </div>
+            <div class="col-9">
+              <input type="text" class="form-control rounded-pill" name="alamat" required>
+            </div>
+          </div>
+          <div class="row mt-2">
+            <div class="col-3">
+              <label for="email">Email</label>
+            </div>
+            <div class="col-9">
+              <input type="text" class="form-control rounded-pill" name="email" required>
+            </div>
+          </div>
+      </div>
+      <div class="modal-footer d-flex justify-content-center">
+        <button type="button" class="btn btn-secondary rounded-pill" data-bs-dismiss="modal">Batalkan</button>
+        <button class="btn btn-primary rounded-pill" type="submit" name="tambah-data-pelanggan" href="#">Simpan</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+<!-- End modal -->
+
 <!-- Modal Bayar -->
 <div class="modal fade" id="pembayaran" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
